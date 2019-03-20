8df96174d93b0f7f8d47c2c505ccda6862cb4de1
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    s5e9925: Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,9 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 include hardware/samsung_slsi-linaro/config/BoardConfig9925.mk
 
+# Properties
+TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/props/vendor.prop
+
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
