a8078a3eb9d2c6fb807ddcb1fd9e5fa668379a99
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    s5e9925: build AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -77,6 +77,10 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_c2.xml \
     media_profiles_V1_0.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider-service.samsung
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
