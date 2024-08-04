26f3830629ff145381e2353dd4512f3f94202931
Tim Zimmermann <tim@linux4.de>
2024-08-04 10:56:38 +0200

    s5e9925: Use pixel thermal HAL directly
    Change-Id: I30e0f13acc5f2ae4d46b4d449c49b925b930c4ca
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -191,5 +191,11 @@ PRODUCT_SOONG_NAMESPACES += \
 	hardware/samsung_slsi-linaro/interfaces \
 	hardware/samsung_slsi-linaro/sgpu
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.pixel \
+    thermal_info_config.json \
+    thermal_symlinks
+
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/s5e9925-common/s5e9925-common-vendor.mk)
