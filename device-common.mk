322abc3165bbec2dd28135d2e6e1b265284e51b0
Tim Zimmermann <tim@linux4.de>
2025-03-23 19:31:18 +0000

    s5e9925: Build cbd from source
    * CPBoot (Core Processor/Modem) Daemon
    
    Change-Id: Ic0f2671e692ada85cbe6562c352f5a5fc583878a
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -198,6 +198,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat
 
+# RIL
+PRODUCT_PACKAGES += \
+    cbd
+
+$(call soong_config_set,cbd,protocol,sipc)
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal \
@@ -212,7 +218,8 @@ PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-	hardware/samsung
+	hardware/samsung \
+    hardware/samsung_slsi-linaro/exynos/cpboot_v3
 
 # TEEgris
 PRODUCT_PACKAGES += \
