922291b52318761b1ca22666cc52bdd4004ab868
Tim Zimmermann <tim@linux4.de>
2025-11-03 17:48:17 +0200

    s5e9925: Define common kernel build flags
    Change-Id: I97fbe2c610fc750e3e79a095ec0710e53e9ea02c
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -7,6 +7,10 @@
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Kernel Modules
+PRODUCT_PACKAGES += \
+    toolbox.vendor_ramdisk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
