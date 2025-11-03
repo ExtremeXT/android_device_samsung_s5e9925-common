1e71bd47b5a7f4a29ba6630cc2020a390e3957d8
ExtremeXT <extremextdev@gmail.com>
2025-11-03 21:29:49 +0200

    s5e9925: Import empty overlays
    Change-Id: I4e17b3b46ff05a9fd3a160ae9a1f932c3475e4f4
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
