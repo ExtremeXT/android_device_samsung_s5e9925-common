97ff5369ef6b19dc6028e871d0ecbb62dbd18e13
ExtremeXT <extremext@lineageos.org>
2025-11-03 17:57:49 +0200

    s5e9925: Import empty VINTF Manifests
    * Use target level 7 as it is the latest available for ACK 5.10
    
    Change-Id: I1ef35ac541ef3950827189cd2df0762e1fc3838e
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,3 +8,9 @@ COMMON_PATH := device/samsung/s5e9925-common
 
 # Security
 VENDOR_SECURITY_PATCH := 2026-02-01
+
+# VINTF
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.device.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
