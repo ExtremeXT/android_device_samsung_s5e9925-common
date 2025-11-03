d17270c79171dc9ac755ce55316bd00e609663a4
ExtremeXT <extremext@lineageos.org>
2025-11-03 17:30:40 +0200

    s5e9925: Define platform-specific flags
    * All of our SoC's core clusters support ARMv9-a instructions.
    
    Change-Id: Iaf5fa7329938008c029570dbce961c666236a527
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Shipping API level
+BOARD_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
