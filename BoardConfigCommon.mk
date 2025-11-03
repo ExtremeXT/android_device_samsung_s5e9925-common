184936507d35e16609e1fee7b8ce67c682468615
ExtremeXT <extremext@lineageos.org>
2025-11-03 17:17:33 +0200

    s5e9925: Setup basic skeleton tree
    Change-Id: Ib556d1acbb6a48265187cde705dc29b2f81405d3
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/s5e9925-common
+
+# Security
+VENDOR_SECURITY_PATCH := 2026-07-01
---
+++ extract-files.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/s5e9925-common',
+    'vendor/samsung/s5e9925-common',
+]
+
+module = ExtractUtilsModule(
+    's5e9925-common',
+    'samsung',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
