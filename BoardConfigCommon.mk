69be73ba2abf52b6ed0f14d47836203234552f8e
ExtremeXT <extremext@lineageos.org>
2025-11-03 17:17:33 +0200

    s5e9925: Setup basic skeleton tree
    Change-Id: Ib556d1acbb6a48265187cde705dc29b2f81405d3
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/s5e9925-common
+
+# Security
+VENDOR_SECURITY_PATCH := 2026-02-01
