8044c8a920da08b10ddbf96f8656b141f6f6406f
Tim Zimmermann <tim@linux4.de>
2025-11-04 15:05:21 +0200

    s5e9925: Import minimal releasetools extension
    * Flash dtbo, vbmeta and vendor_boot images
    
    Change-Id: I9cbeca7207247edb768258db3132b7bacd21bb2c
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

---
+++ releasetools/releasetools.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import common
+import re
+
+def FullOTA_InstallEnd(info):
+    OTA_InstallEnd(info)
+    return
+
+def IncrementalOTA_InstallEnd(info):
+    info.input_zip = info.target_zip
+    OTA_InstallEnd(info)
+    return
+
+def AddImage(info, basename, dest):
+    data = info.input_zip.read("IMAGES/" + basename)
+    common.ZipWriteStr(info.output_zip, basename, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+    AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+    AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+    AddImage(info, "vendor_boot.img", "/dev/block/by-name/vendor_boot")
+    return
