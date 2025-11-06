e44498433129aeb6b7d0170ae0f8e65aaa1c0128
Tim Zimmermann <tim@linux4.de>
2025-11-06 18:04:16 +0200

    s5e9925: Setup Keymint
    Change-Id: I8ea2a3d0f225765a18f33d4b50609050b194ec54
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -21,6 +26,33 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hermesd': blob_fixup()
         .binary_regex_replace(b'security.securehw.available', b'vendor.s.securehw.available')
         .binary_regex_replace(b'security.securenvm.available', b'vendor.s.securenvm.available'),
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
+        'vendor/lib64/lib_android_keymaster_skeymint_utils.so',
+        'vendor/lib64/libskeymint.so',
+        'vendor/lib64/libskeymint10device.so',
+        'vendor/lib64/libskeymint_cli.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
+            'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
+             'android.hardware.security.sharedsecret-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
+        .add_needed('libbase_shim.so')
+        .replace_needed('libkeymint.so', 'libskeymint.so')
+        .replace_needed('lib_android_keymaster_keymint_utils.so',
+            'lib_android_keymaster_skeymint_utils.so')
+        .replace_needed('libkeymaster_portable.so',
+            'libkeymaster_portable.samsung.so'),
+    'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup()
+        .regex_replace('-service', '-service.samsung'),
+    'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
