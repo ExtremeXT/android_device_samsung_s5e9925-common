fc867174eff451158ad8ce02defb44e35e082682
ExtremeXT <extremext@lineageos.org>
2025-11-07 16:16:49 +0200

    s5e9925: Import required Pixel Hardware soong namespaces
    Change-Id: Ib886f66d984be1a95ba0ec5198e758e542a069a8
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -173,6 +173,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     bootable/deprecated-ota \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/samsung \
     hardware/samsung_slsi-linaro/exynos/libaudio/audiohal_comv1 \
     hardware/samsung_slsi-linaro/exynos/libaudio/audiohal_comv1/proxy
