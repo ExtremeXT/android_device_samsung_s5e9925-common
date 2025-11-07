5b895bacd5b4179b35e6d1ed0979747478681a56
ExtremeXT <extremext@lineageos.org>
2025-11-07 15:05:35 +0200

    s5e9925: Build SGR Gralloc HIDL 4.0
    Change-Id: I2f6f0716d86e63107ca103672109f3e49ce7198f
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -67,7 +67,9 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.composer.hwc3-service.slsi
+    android.hardware.composer.hwc3-service.slsi \
+    android.hardware.graphics.allocator@4.0-service-sgr \
+    android.hardware.graphics.mapper@4.0-impl-sgr
 
 # Health
 PRODUCT_PACKAGES += \
