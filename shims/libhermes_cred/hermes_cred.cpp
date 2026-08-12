17cfc2b491aff458fcf593a11238d88745c6c749
ExtremeXT <extremext@lineageos.org>
2026-08-12 06:55:01 +0300

    s5e9925: Switch to OSS hermesd implementation
    * And remove weaver
    
    Change-Id: If7cc23f2018e46df6fabd90867d76cd0c35797cf
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ shims/libhermes_cred/hermes_cred.cpp
@@ -0,0 +1,10 @@
+namespace android::hardware {
+template <typename> struct hidl_vec {
+};
+}
+
+using android::hardware::hidl_vec;
+
+unsigned int hermes_put_persistent_cred(hidl_vec<unsigned char>&, hidl_vec<unsigned char>&) {
+    return 1000; // "hermes_cred", "!@ %s(%d) cannot put persistent cred hal"
+}
